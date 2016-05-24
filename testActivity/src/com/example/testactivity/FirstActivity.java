845f026e65bc84964b3298fd142492a3eb7eecc5
wusitan <1092921638@qq.com>
2016-05-24 16:08:53 +0800

    新家的看看

---
+++ testActivity/src/com/example/testactivity/FirstActivity.java
@@ -0,0 +1,11 @@
+package com.example.testactivity;
+
+import android.app.Activity;
+
+public class FirstActivity extends Activity {
+	private void syso() {
+		// TODO 自动生成的方法存根
+		System.out.print("nihao ");
+	}
+
+}
